0f8021452e9c253c8847be305d9e48bb7a0b1952
Asheesh Laroia <asheesh@asheesh.org>
2023-06-12 20:59:44 -0400

    Add initial direction handling

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,12 @@ use core::{arch::wasm32, panic::PanicInfo};
 
 const GAMEPAD1: *const u8 = 0x16 as *const u8;
 
-const BUTTON_LEFT: u8 = 16;  // 00010000
-const BUTTON_RIGHT: u8 = 32; // 00100000
-const BUTTON_UP: u8 = 64;    // 01000000
+const BUTTON_LEFT: u8 = 1 << 4;
+// 00010000
+const BUTTON_RIGHT: u8 = 32;
+// 00100000
+const BUTTON_UP: u8 = 64;
+// 01000000
 const BUTTON_DOWN: u8 = 128; // 10000000
 
 extern "C" {
@@ -22,5 +25,69 @@ fn phandler(_: &PanicInfo<'_>) -> ! {
 unsafe fn update() {
     if *GAMEPAD1 & BUTTON_UP != 0 {
         vline(80, 20, 120);
+    }
+}
+
+const MAP: [u16; 8] = [
+    0b1111111111111111,
+    0b1000001010000101,
+    0b1011100000110101,
+    0b1000111010010001,
+    0b1010001011110111,
+    0b1011101001100001,
+    0b1000100000001101,
+    0b1111111111111111,
+];
+
+/// Check if the map contains a wall wat a point.
+fn point_in_wall(x: f32, y: f32) -> bool {
+    match MAP.get(y as usize) {
+        Some(line) => (line & (0b1 << x as usize) != 0),
+        None => true,
+    }
+}
+
+struct State {
+    player_x: f32,
+    player_y: f32,
+    player_angle: f32,
+}
+
+static mut STATE: State = State {
+    player_x: 1.5,
+    player_y: 1.5,
+    player_angle: 0.0,
+};
+
+const STEP_SIZE: f32 = 0.045;
+
+impl State {
+    /// move the character
+    pub fn update(&mut self, up: bool, down: bool, left: bool, right: bool) {
+        // store our current position in case we might need it later
+        let previous_position = (self.player_x, self.player_y);
+
+        if up {
+            self.player_x += cosf(self.player.angle) * STEP_SIZE;
+            self.player_x += -sinf(self.player_angle) * STEP_SIZE;
+        }
+
+        if down {
+            self.player_x -= cosf(self.player_angle) * STEP_SIZE;
+            self.player_y -= sinf(self.player_angle) * STEP_SIZE;
+        }
+
+        if left {
+            self.player_angle += STEP_SIZE;
+        }
+
+        if right {
+            self.player_angle -= STEP_SIZE;
+        }
+
+        // if we hit a wall, revert
+        if point_in_wall(self.player_x, self.player_y) {
+            (self.player_x, self.player_y) = previous_position;
+        }
     }
 }
